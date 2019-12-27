6920399207bb8dc2c26d93863f19cb55c57b5d72
Kouki Mita <s1F101700338@iniad.org>
2019-12-28 00:35:59 +0900

    last

---
+++ app/src/main/java/com/example/kankouapp2/inokashira_park.java
@@ -0,0 +1,14 @@
+package com.example.kankouapp2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class inokashira_park extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inokashira_park);
+    }
+}
